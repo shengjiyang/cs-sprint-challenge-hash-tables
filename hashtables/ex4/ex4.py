b6b75544d3af02735553d89aabb09715db83785e
shengjiyang <samswank@gmail.com>
2020-08-07 10:16:45 -0600

    3/4 tests on ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,24 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+import numpy as np
 
-    return result
+def has_negatives(a):
+    d = {}
+    for i in a:
+        if a.count(-i) > 0:
+            d[i] = i
+
+    if np.where(np.array(a) < 0) == np.array([]):
+        return []
+
+    else:
+        return [i for i in d if i > 0]
 
 
 if __name__ == "__main__":
+    print(has_negatives([1,2,3]))
+    print(has_negatives([1,2,3,-4]))
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+    # a = list(range(5000000))
+    # a += [-1,-2,-3]
+
+    # print(has_negatives(a))
