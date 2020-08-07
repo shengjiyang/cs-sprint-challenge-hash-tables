89d0035d652ac6fe9d6a285659e15ba06e51aeec
shengjiyang <samswank@gmail.com>
2020-08-07 11:11:43 -0600

    ex2 tests passed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,26 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    d = {}
+    for ticket in tickets:
+        d[ticket.source] = ticket.destination
+
+    route = []
+    for ticket in d.keys():
+        if ticket == "NONE":
+            route.append(d[ticket])
+
+    for i in range(length - 1):
+        route.append(d[route[i]])
 
     return route
+
+
+if __name__ == "__main__":
+    ticket_1 = Ticket("NONE", "PDX")
+    ticket_2 = Ticket("PDX", "DCA")
+    ticket_3 = Ticket("DCA", "NONE")
+
+    tickets = [ticket_1, ticket_2, ticket_3]
+
+    print(reconstruct_trip(tickets, length=len(tickets)))
